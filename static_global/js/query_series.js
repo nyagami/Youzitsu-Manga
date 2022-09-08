9e245aedbf9ff60918076e2ac2cf12043ece72e7
Nyagami <hoangquan05112002@gmail.com>
2022-09-08 23:03:19 +0700

    small fix

--- static_global/js/query_series.js
+++ static_global/js/query_series.js
@@ -17,12 +17,12 @@ query_series = fetch(`/api/${api}/`).then(res => res.json()).then(series_slug =>
                 if(i==0){
                 document.getElementById("title-container").innerHTML=`<a class="text-decoration-none" href="/read/manga/${series.slug}/"><h1 class="series-title">${series.title}</h1></a>`
                 document.getElementById("content-container").innerHTML=`<div class="series-content">${series.description}</div>`
-                document.getElementById("footer-container").innerHTML =`<div><a href="#">${series.author}</a> | <a href="#">${series.artist}</a> | <a href="#">${series.groups[1]}</a></div>`
+                document.getElementById("footer-container").innerHTML =`<div><a href="#">${series.author}</a> | <a href="#">${series.artist}</a></div>`
             }
             else{
                 document.getElementById("title-container").innerHTML +=`<a class="d-none text-decoration-none" href="/read/manga/${series.slug}/"><h1 class="series-title">${series.title}</h1></a>`
                 document.getElementById("content-container").innerHTML +=`<div class="series-content d-none">${series.description}</div>`
-                document.getElementById("footer-container").innerHTML +=`<div class="d-none"><a class="d-none" href="#">${series.author}</a> | <a href="#">${series.artist}</a> | <a href="#">${series.groups[1]}</a></div>`
+                document.getElementById("footer-container").innerHTML +=`<div class="d-none"><a class="d-none" href="#">${series.author}</a> | <a href="#">${series.artist}</a></div>`
             }
         })
         fetch(`/api/${get_cover_gender}/${series_slug[i]}/`).then(res => res.json()).then(covers => {
