8756ae6f66d502332e7b28131aeb5608a25697ef
Nyagami <hoangquan05112002@gmail.com>
2022-09-09 15:54:17 +0700

    small fix

--- static_global/js/page.js
+++ static_global/js/page.js
@@ -35,7 +35,7 @@ function change_book(step){
 }
 
 function set_background(){
-    current_image = document.getElementById(`book-container`).childNodes[current_book].childNodes[current_page[current_book]].firstChild.src.slice(window.location.href.length).split('.')[0]
+    current_image = document.getElementById(`book-container`).childNodes[current_book].childNodes[current_page[current_book]].firstChild.lastElementChild.src.slice(window.location.href.length).split('.')[0]
     document.getElementById("page-model-background").style.backgroundImage = `url(${current_image}_blur.jpeg)`
     document.getElementById("link-full-img").setAttribute("href",`${current_image}.jpeg`)
     link_to_read = document.getElementById("title-container").childNodes[current_book].href
