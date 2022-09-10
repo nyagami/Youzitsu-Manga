98107c15eb78221f6389f981cdde38a0e8307f0a
Raikion201 <haitrieukt123@gmail.com>
2022-09-10 13:06:41 +0700

    Page err

--- homepage/views.py
+++ homepage/views.py
@@ -122,7 +122,7 @@ def random(request):
 
 
 def handle404(request, exception):
-    return render(request, "homepage/how_cute_404.html", status=404)
+    return render(request, "homepage/err/404_page.html", status=404)
 
 def handle500(request):
-    return render(request, "homepage/thonk_500.html", status=500)
+    return render(request, "homepage/err/500_page.html", status=500)
