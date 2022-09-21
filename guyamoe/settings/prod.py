e1e5492e58edf5f58dea9c6f1b7420a5b6b070af
Nyagami <hoangquan05112002@gmail.com>
2022-09-21 16:14:30 +0700

    change dbms

--- guyamoe/settings/prod.py
+++ guyamoe/settings/prod.py
@@ -53,7 +53,7 @@ CACHES = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
