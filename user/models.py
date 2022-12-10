06c3c2ab7f24bf8477b3c8da60d85687edae32de
Nyagami <hoangquan05112002@gmail.com>
2022-12-10 13:47:52 +0700

    login link

--- user/models.py
+++ user/models.py
@@ -11,7 +11,7 @@ def avatar_image_path(self, filename):
     return os.path.join("user", "avatar", self.user.username, filename)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user", related_name="user")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user", related_name="profile")
     display_name = models.CharField(verbose_name="Tên hiển thị", max_length=50, blank=True, null=True)
     avatar = models.ImageField(upload_to=avatar_image_path, blank=True)
     description = models.TextField(verbose_name="Mô tả", max_length=500, blank=True)
