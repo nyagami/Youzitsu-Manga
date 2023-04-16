5dad8d0407152362b633f15f7bd5f672fb012082
Nyagami <hoangquan05112002@gmail.com>
2023-04-16 14:49:24 +0700

    complete consumers

--- utils/consumers.py
+++ utils/consumers.py
@@ -1,5 +1,6 @@
 
 # from channels.db import database_sync_to_async
+from channels.consumer import get_channel_layer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
@@ -20,11 +21,26 @@ class NotifcationConsumer(AsyncJsonWebsocketConsumer):
 
     async def receive_json(self, content, **kwargs):
         notifcation = content['notification']
-        notifcation
+        self.channel_layer.group_send(
+            self.group,
+            {'type': 'notify', 'notifcation': notifcation}
+        )
+
+    async def notify(self, event, close=False):
+        await self.send_json(event)
+
+    # content must be decoded json
+    async def notify_all(self, content, users):
+        for user in users:
+            await get_channel_layer().group_send(
+                user.username,
+                {'type': 'notify', 'notification': content}
+            )
 
 
 class CommentConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
+        # group should be like ^[a-z]_[\w-]+
         self.group = self.scope['url_route']['kwargs']['type'] + '_' + self.scope['url_route']['kwargs']['article']
         await self.channel_layer.group_add(
             self.group,
@@ -40,4 +56,10 @@ class CommentConsumer(AsyncJsonWebsocketConsumer):
 
     async def receive_json(self, content, **kwargs):
         comment = content['comment']
-        comment
+        await self.channel_layer.group_send(
+            self.group,
+            {'type': 'comment', 'comment': comment}
+        )
+
+    async def comment(self, event):
+        await self.send_json(event)
