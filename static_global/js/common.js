5905a78b0057a001ae5ba4625e7cc80a0d5941ce
Nyagami <hoangquan05112002@gmail.com>
2023-04-22 14:47:24 +0700

    CommentSocket class

--- static_global/js/common.js
+++ static_global/js/common.js
@@ -187,10 +187,6 @@ function CommentBox(node, data, isRootComment){
             e.stopImmediatePropagation();
         });
 
-        // hack
-        const img = this.img;
-        const deleteBtn = this.deleteBtn;
-
         this.textarea.addEventListener("paste", (event) => {
             const items = event.clipboardData.items;
             for(let i in items){
@@ -198,9 +194,9 @@ function CommentBox(node, data, isRootComment){
                 if(item.kind === 'file' && (['image/png', 'image/jpeg', 'image/webp'].includes(item.type))){
                     const blob = item.getAsFile();
                     const reader = new FileReader();
-                    reader.onload = function(e){
-                        img.setAttribute('src', e.target.result);
-                        deleteBtn.classList.remove("hidden");
+                    reader.onload = (e) => {
+                        this.img.setAttribute('src', e.target.result);
+                        this.deleteBtn.classList.remove("hidden");
                     }
                     reader.readAsDataURL(blob);
                 }
@@ -212,9 +208,9 @@ function CommentBox(node, data, isRootComment){
             if(this.imgInput.files && this.imgInput.files[0]){
                 const reader = new FileReader();
     
-                reader.onload = function (e) {
-                    img.setAttribute('src', e.target.result);
-                    deleteBtn.classList.remove("hidden");
+                reader.onload = (e) => {
+                    this.img.setAttribute('src', e.target.result);
+                    this.deleteBtn.classList.remove("hidden");
                 };
     
                 reader.readAsDataURL(this.imgInput.files[0]);
@@ -268,5 +264,60 @@ function ImageModal(){
     this.init();
 }
 
+function CommentSocket(){
+    this.socket = null;
+    this.ws_url = `ws://${window.location.host}/ws/comment/c/${article}/`;
+    this.open = () => {
+        this.socket = new WebSocket(this.ws_url);
+        this.socket.onopen = () => {
+            console.log("Websocket opened for Comment");
+        }
+        this.socket.onclose = () => {
+            setTimeout(function () {
+                console.log("Reconnecting...");
+                this.open();
+            }, 2000);
+        }
+        this.socket.onmessage = e => {
+            const data = JSON.parse(e.data);
+            switch (data.type) {
+                case 'comment':
+                    const comment = data.comment;
+                    const commentElement = document.createElement('li');
+                        commentElement.innerHTML = `
+                        <div class="comment-container deepth-${comment.deepth}"
+                            data-id="${comment.id}" data-parent="${comment.parent || ''}" data-deepth="${comment.deepth}"
+                            data-time="${comment.created_on}" data-username="${comment.username}"
+                            data-display-name="${comment.author.display_name}" data-avatar="${comment.author.avatar}"
+                            data-content="${comment.content}" data-media="${comment.media_url}"
+                            data-bind="comment_node"
+                        >
+                        </div>
+                        <ul class="comment-list" comment-id="${comment.id}"></ul>
+                        <div class="comment-box-container" comment-id="${comment.id}"></div>
+                        `;
+                    if(comment.parent){
+                        const parent = document.querySelector(`ul.comment-list[comment-id="${comment.parent}"]`);
+                        parent.appendChild(commentElement);
+                    }else{
+                        const wrapper = document.querySelector("ul.comment-list[comment-id='-1']");
+                        wrapper.prepend(commentElement);
+                    }
+                    new CommentNode(commentElement.querySelector("[data-bind='comment_node']"));
+                    break;
+                default:
+                    break;
+            }
+        }
+        this.socket.onerror = err => {
+            console.log("WebSocket encountered an error: " + err.message);
+            console.log("Closing the socket.");
+            this.socket.close();
+        }
+    }
+    this.open();
+}
 
 imageModal = new ImageModal();
+
+new CommentSocket();
